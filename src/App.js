a796d4a32395257753b1064bed9da91d4e9a0ae2
hiroko abe <tora.cnch0205@gmail.com>
2019-06-22 16:31:43 +0900

    learn prop-types

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { ninvoke } from 'q';
 
 const App = () => {
   const profiles = [
     { name: "Taro", age: 10 },
     { name: "Hanako", age: 90 },
-    { name: "jiro", age: 8 },
-    { name: "noName" },
+    { name: "jiro", age: ninvoke;; },
   ]
   return (
     <div>
@@ -22,8 +23,9 @@ const User = (props) => {
   return <div>Hi, I am {props.name}, and {props.age} years old.</div>
 }
 
-User.defaultProps = {
-  age: 1
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired,
 }
 
 export default App;
