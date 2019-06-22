6243527100ac0ffb87109e6a01e45a359790605d
hiroko abe <tora.cnch0205@gmail.com>
2019-06-22 16:41:06 +0900

    fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ninvoke } from 'q';
 
 const App = () => {
   const profiles = [
     { name: "Taro", age: 10 },
     { name: "Hanako", age: 90 },
-    { name: "jiro", age: ninvoke;; },
+    { name: "jiro", age: 8 },
   ]
   return (
     <div>
